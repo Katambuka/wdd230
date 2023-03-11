25703e58245f959491bc4f6ffda65070c43f8143
Katambuka <kan22001@byui.edu>
2023-03-11 21:37:01 +0400

    Newupdates

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,19 +1,41 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#caption-desc');
+const city = "Dubai";
+const units = "metric";
 
-// Get the temperature and wind speed values
-const temperature = 5; // replace with the actual temperature value
-const windSpeed = 10; // replace with the actual wind speed value
+// replace {lat}, {lon}, and {api_key} with actual values
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=aae80cacc204382d239af2c8dfeb0af9`;
 
-// Check if the temperature and wind speed meet the specification limits
-if (temperature <= 50 && windSpeed > 3.0) {
-  // Calculate the wind chill factor using the formula
-  const windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
-  
-  // Update the HTML with the wind chill value
-  const windChillElement = document.getElementById("windChill");
-  windChillElement.textContent = `${windChill.toFixed(2)}°C`;
-} else {
-  // The input values did not meet the requirements, so display "N/A"
-  const windChillElement = document.getElementById("windChill");
-  windChillElement.textContent = "N/A";
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      //console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
 }
 
+apiFetch();
+
+function  displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+
+  const windSpeed = weatherData.wind.speed * 3.6; // convert from m/s to km/h
+  const windSpeedElement = document.querySelector('#windSpeed');
+  windSpeedElement.textContent = windSpeed.toFixed(1);
+}
+ // last modified date
+ document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
--- lesson10/scripts/testapi.js
+++ lesson10/scripts/testapi.js
@@ -1,30 +1,59 @@
-const API_KEY = 'aae80cacc204382d239af2c8dfeb0af9';
-const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=${API_KEY}`;
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const city = "Dubai"; // Updated city variable to Dubai
+const units = "imperial";
 
-const weatherIconBaseUrl = 'http://openweathermap.org/img/wn/';
+// replace {lat}, {lon}, and {api_key} with actual values
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=aae80cacc204382d239af2c8dfeb0af9`;
 
-fetch(API_URL)
-  .then(response => response.json())
-  .then(data => {
-    const weatherDescription = data.weather[0].description;
-    const weatherIconCode = data.weather[0].icon;
-    const temperature = Math.round(data.main.temp - 273.15); // Convert from Kelvin to Celsius
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      //console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
 
-    const weatherIcon = document.createElement('img');
-    weatherIcon.src = `${weatherIconBaseUrl}${weatherIconCode}@2x.png`;
+apiFetch();
 
-    const weatherDescriptionElement = document.createElement('div');
-    weatherDescriptionElement.textContent = weatherDescription;
+function displayResults(weatherData) {
+  // Display current temperature
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
 
-    const temperatureElement = document.createElement('div');
-    temperatureElement.textContent = `${temperature}°C`;
+  // Display current condition
+  const condition = weatherData.weather[0].description;
+  captionDesc.textContent = condition;
 
-    const weatherIconContainer = document.getElementById('weather-icon');
-    const weatherDescriptionContainer = document.getElementsByTagName('figcaption')[0];
-    const temperatureContainer = document.getElementById('current-temp');
+  // Display weather icon
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', condition);
 
-    weatherIconContainer.parentNode.insertBefore(weatherIcon, weatherIconContainer);
-    weatherDescriptionContainer.appendChild(weatherDescriptionElement);
-    temperatureContainer.appendChild(temperatureElement);
-  })
-  .catch(error => console.error(error));
+  // Display wind speed
+  const windSpeed = weatherData.wind.speed.toFixed(1);
+  const windSpeedElement = document.querySelector('#wind-speed');
+  windSpeedElement.textContent = `Wind Speed: ${windSpeed} mph`;
+
+  // Display wind chill
+  const temp = weatherData.main.temp;
+  const windChill = calculateWindChill(temp, windSpeed);
+  const windChillElement = document.querySelector('#wind-chill');
+  windChillElement.textContent = `Wind Chill: ${windChill}`;
+}
+
+function calculateWindChill(temp, speed) {
+  // Return "N/A" if wind chill is not applicable
+  if (temp > 50 || speed < 3) {
+    return "N/A";
+  }
+  const windChill = 35.74 + (0.6215 * temp) - (35.75 * (speed ** 0.16)) + (0.4275 * temp * (speed ** 0.16));
+  return windChill.toFixed(0);
+}
