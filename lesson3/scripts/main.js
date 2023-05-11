7685e1361e89a28f306c2f57dc2925ccf94eed84
Katambuka <kan22001@byui.edu>
2023-05-12 02:40:47 +0400

    nhj

--- lesson3/scripts/main.js
+++ lesson3/scripts/main.js
@@ -1,3 +1,10 @@
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+const x = document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu;
+
 const options = {
    weekdays: "long",
    day: "numeric",
--- script/main.js
+++ script/main.js
@@ -1,9 +1,4 @@
-function toggleMenu() {
-  document.getElementById("primaryNav").classList.toggle("open");
-  document.getElementById("hamburgerBtn").classList.toggle("open");
-}
-const x = document.getElementById('hamburgerBtn')
-x.onclick = toggleMenu;
+
 const options = {
   weekdays: "long",
   day: "numeric",
