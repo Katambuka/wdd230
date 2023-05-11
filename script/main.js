d1e6438e7ed9637ffe10b39456dc8dfb2065945f
Katambuka <kan22001@byui.edu>
2023-05-12 02:22:38 +0400

    hamburgerbtn

--- script/main.js
+++ script/main.js
@@ -1,3 +1,9 @@
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+const x = document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu;
 const options = {
   weekdays: "long",
   day: "numeric",
