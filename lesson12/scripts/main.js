d9982e062983b909cffba5217442c21ea5e5d0b9
Katambuka <kan22001@byui.edu>
2023-07-21 21:15:04 +0400

    changes

--- lesson12/scripts/main.js
+++ lesson12/scripts/main.js
@@ -102,3 +102,42 @@ fetch(
   .catch((error) => {
     console.error(error);
   });
+
+  /****lazyloading*****/
+  document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll("[data-src]");
+  
+    images.forEach((image) => {
+      const placeholderSrc = "images/placeholder.png";
+      image.setAttribute("src", placeholderSrc);
+    });
+  
+    const imgOptions = {
+      threshold: 0,
+      rootMargin: "0px 0px -50px 0px",
+    };
+  
+    const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+          return;
+        } else {
+          preloadImage(entry.target);
+          imgObserver.unobserve(entry.target);
+        }
+      });
+    }, imgOptions);
+  
+    images.forEach((image) => {
+      imgObserver.observe(image);
+    });
+  });
+  
+  function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+      return;
+    }
+    img.src = src;
+  }
+  
